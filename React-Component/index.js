2276b30f719e4d2577b3baa69f84be157e06ef9a
hari-hs <165835844+hari-hs@users.noreply.github.com>
2024-08-10 18:52:44 -0500

    Add files via upload

---
+++ React-Component/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header/Header';
+export { default as Footer } from './Footer/Footer';
+export { default as Modal } from './Modal/Modal';
+export { default as AboutUs } from './AboutUs/AboutUs';
