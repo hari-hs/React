bafc82c13725fdcf7f4cfa938bdcfec8a954bf61
hari-hs <165835844+hari-hs@users.noreply.github.com>
2024-08-11 14:37:39 -0500

    Delete React-Component directory

--- React-Component/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header/Header';
-export { default as Footer } from './Footer/Footer';
-export { default as Modal } from './Modal/Modal';
-export { default as AboutUs } from './AboutUs/AboutUs';
